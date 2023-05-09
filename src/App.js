8e46540ede58baa603efe59ede8e1a170ad60a5d
Djamalutin <fisher.gerryy@mail.ru>
2023-05-09 15:31:37 +0300

    feature: adding useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./index.scss";
 // import Success from "./components/Sucsess";
 import { Users } from "./components/Users";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users`)
+      .then((res) => res.json)
+      .then((json) => setUsers(json.data))
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей!");
+      })
+      .finally(() => {
+        setLoading(true);
+      });
+  }, []);
   return (
     <div className="App">
       <Users />
