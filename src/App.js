031a67bd614ddef1b14cbec995e15ca1700b3ef8
Djamalutin <fisher.gerryy@mail.ru>
2023-05-09 13:56:59 +0300

    feature: finished buiding application

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Success from "./components/Sucsess";
 import { Users } from "./components/Users";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Users />
+    </div>
+  );
 }
 
 export default App;
